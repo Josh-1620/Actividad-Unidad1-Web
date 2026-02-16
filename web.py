35053fafffae98b0db680af616e46c89820b94d0
Joshua Ismael Salcedo Beltrán <l20211842@tectijuana.edu.mx>
2026-02-16 07:52:21 +0000

    cambiando el get

--- web.py
+++ web.py
@@ -10,10 +10,22 @@ class WebRequestHandler(BaseHTTPRequestHandler):
         return dict(parse_qsl(self.url().query))
 
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-Type", "text/html")
-        self.end_headers()
-        self.wfile.write(self.get_response().encode("utf-8"))
+    parsed_url = urlparse(self.path)
+    path_parts = parsed_url.path.strip("/").split("/")
+    query = dict(parse_qsl(parsed_url.query))
+
+    if len(path_parts) == 2 and path_parts[0] == "proyecto":
+        proyecto = path_parts[1]
+        autor = query.get("autor", "")
+        response = f"<h1>Proyecto: {proyecto} Autor: {autor}</h1>"
+    else:
+        response = "<h1>Ruta no encontrada</h1>"
+
+    self.send_response(200)
+    self.send_header("Content-Type", "text/html")
+    self.end_headers()
+    self.wfile.write(response.encode("utf-8"))
+
 
     def get_response(self):
         return f"""
