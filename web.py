51748526c94de0e68280b4e6a4e4e1c1198c16f4
Joshua Ismael Salcedo Beltrán <l20211842@tectijuana.edu.mx>
2026-02-16 07:57:40 +0000

    eliminando response

--- web.py
+++ web.py
@@ -30,8 +30,3 @@ class WebRequestHandler(BaseHTTPRequestHandler):
    
 
 
-if __name__ == "__main__":
-    PORT = 8000
-    server = HTTPServer(("localhost", PORT), WebRequestHandler)
-    print(f"Starting server on port {PORT}")
-    server.serve_forever()
